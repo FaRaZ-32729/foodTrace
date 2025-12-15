e43b38fa357a99c6608560e402a4879c07eab09e
FaRaZ-32729 <sial32729@gmail.com>
2025-12-15 20:06:49 +0500

    device model

--- src/models/deviceModel.js
+++ src/models/deviceModel.js
@@ -4,7 +4,7 @@ const conditionSchema = new mongoose.Schema({
   type: {
     type: String,
     required: true,
-    enum: ["temperature", "humidity"],
+    enum: ["freezer", "ambient"],
   },
   operator: {
     type: String,
