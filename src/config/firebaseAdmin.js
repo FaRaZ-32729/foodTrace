48f07be2387561a5acfc7d7839b9882abc45fc25
FaRaZ-32729 <sial32729@gmail.com>
2025-12-18 22:22:57 +0500

    firebase remaining

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,10 +1,10 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseKey.json");
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./firebaseKey.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "frost-control-97eb3.firebasestorage.app",
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     storageBucket: "frost-control-97eb3.firebasestorage.app",
+// });
 
-const bucket = admin.storage().bucket();
-module.exports = bucket;
+// const bucket = admin.storage().bucket();
+// module.exports = bucket;
